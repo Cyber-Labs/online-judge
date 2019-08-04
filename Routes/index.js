f59a7669aabc8f153f425c27f5b25d7ca49837bd
NIKHIL BN <nikhilbnaradhya1.gem@gmail.com>
2019-08-04 22:29:16 +0530

    Del node_modules

--- Routes/index.js
+++ Routes/index.js
@@ -7,7 +7,7 @@ app.post('/login', auth.login);
 app.post('/signup', auth.signup);
 app.post('/users/:username/forgot_password', auth.forgotPassword);
 app.post('/users/:username/update_password', auth.updatePassword);
-app.post('/users/:username/edit_user', user.editUser);
+app.post('/users/:username/update_user', user.editUser);
 app.get('/users/:username', user.getUser);
 
 module.exports = app;
