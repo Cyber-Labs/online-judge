f09b4d2c551fe220095adccb8a3348d4fbb2ad36
NIKHIL BN <nikhilbnaradhya1.gem@gmail.com>
2019-08-05 15:17:16 +0530

    verifyzAccessToken middleware created to verify the access token provided by the user

--- Routes/index.js
+++ Routes/index.js
@@ -5,9 +5,21 @@ const app = express();
 
 app.post('/login', auth.login);
 app.post('/signup', auth.signup);
-app.post('/users/:username/forgot_password', auth.forgotPassword);
-app.post('/users/:username/update_password', auth.updatePassword);
-app.post('/users/:username/update_user', user.editUser);
+app.post(
+    '/users/:username/forgot_password',
+    auth.verifyUser.verifyAccessToken,
+    auth.forgotPassword
+);
+app.post(
+    '/users/:username/update_password',
+    auth.verifyUser.verifyAccessToken,
+    auth.updatePassword
+);
+app.post(
+    '/users/:username/update_user',
+    auth.verifyUser.verifyAccessToken,
+    user.editUser
+);
 app.get('/users/:username', user.getUser);
 
 module.exports = app;
