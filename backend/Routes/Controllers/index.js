9c776620b408666496495290ded496cb37f315bf
Sourav Prasad <souravpd007@gmail.com>
2019-08-14 19:05:43 +0530

    Added prettier

--- backend/Routes/Controllers/index.js
+++ backend/Routes/Controllers/index.js
@@ -1,7 +1,7 @@
-const authRouter = require('./auth');
-const express = require('express');
+const authRouter = require("./auth");
+const express = require("express");
 const router = express.Router();
 
-router.use('/auth', authRouter);
+router.use("/auth", authRouter);
 
 module.exports = router;
