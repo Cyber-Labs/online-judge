0255c5975c4b9a1bee6fc0acbb78da743cbcb757
ridhishjain <ultimate.ridhish@gmail.com>
2020-01-03 20:22:09 +0530

    updating

--- backend/Models/contests/submissions/submitMCQ.js
+++ backend/Models/contests/submissions/submitMCQ.js
@@ -12,58 +12,64 @@ const { pool } = require('../../db');
  * @return {Promise}
  */
 
-const calculateScore = (userSolution, negativeMarking, partialMarking, maxMarks) => {
+function calculateScore(
+  userSolution,
+  negativeMarking,
+  partialMarking,
+  maxMarks
+) {
   return new Promise((resolve, reject) => {
     pool.query(
-    `SELECT solution AS solution
+      `SELECT solution AS solution
     FROM questions
     WHERE contest_id = ? AND question_id = ?`,
-    [contestId, questionId],
-    (error, results) => {
-      if (error) {
-        return reject(error);
-      }
-      const actualSolution = results[0].solution;
-
-      if (userSolution === actualSolution) {
-        return resolve(maxMarks);
-      }
-
-      userSolution = userSolution.toUpperCase().split(',');
-      actualSolution = actualSolution.toUpperCase();
-      const userLength = userSolution.length;
-
-      if (negativeMarking === 0 && partialMarking === 0) {
-        return resolve(0);
-      }
-
-      if (negativeMarking === 0 && partialMarking !== 0) {
-        let count = 0;
-        let score = 0;
-        while (count < userLength) {
-          const position = actualSolution.indexOf(userSolution[count]);
-          if (position !== -1) {
-            score += partialMarking;
-          }userOutput, negativeMarking, partialMarking, maxMarks
-          count++;
+      [contestId, questionId],
+      (error, results) => {
+        if (error) {
+          return reject(error);
         }
-        count = 0;
-        score = 0;
-        while (count < userLength) {
-          const position = actualSolution.indexOf(userSolution[count]);
-          if (position === -1) {
-            score += negativeMarking;
+        const actualSolution = results[0].solution;
+
+        if (userSolution === actualSolution) {
+          return resolve(maxMarks);
+        }
+
+        userSolution = userSolution.toUpperCase().split(',');
+        actualSolution = actualSolution.toUpperCase();
+        const userLength = userSolution.length;
+
+        if (negativeMarking === 0 && partialMarking === 0) {
+          return resolve(0);
+        }
+
+        if (negativeMarking === 0 && partialMarking !== 0) {
+          let count = 0;
+          let score = 0;
+          while (count < userLength) {
+            const position = actualSolution.indexOf(userSolution[count]);
+            if (position !== -1) {
+              score += partialMarking;
+            }
+            userOutput, negativeMarking, partialMarking, maxMarks;
+            count++;
           }
-          score += partialMarking;
+          count = 0;
+          score = 0;
+          while (count < userLength) {
+            const position = actualSolution.indexOf(userSolution[count]);
+            if (position === -1) {
+              score += negativeMarking;
+            }
+            score += partialMarking;
+          }
+          return resolve(score);
         }
-        return resolve(score);
       }
-    }
-   )
+    );
   });
 }
 
-async function submitMCQ({
+function submitMCQ({
   contestId,
   questionId,
   questionName,
@@ -71,10 +77,15 @@ async function submitMCQ({
   userOutput,
   negativeMarking,
   partialMarking,
-  maxMarks,
+  maxMarks
 }) {
-  return new Promise((resolve, reject) => {
-    const score = await calculateScore(userOutput, negativeMarking, partialMarking, maxMarks);
+  return new Promise(async (resolve, reject) => {
+    const score = await calculateScore(
+      userOutput,
+      negativeMarking,
+      partialMarking,
+      maxMarks
+    );
     pool.query(
       `INSERT INTO submissions
       (contest_id, question_id, question_name username, user_ouput, score)
