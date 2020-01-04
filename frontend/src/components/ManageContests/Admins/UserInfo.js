c287081fd17bf6d397785dc827b03851b62db0fe
cjchirag7 <cjchirag7@gmail.com>
2020-01-04 16:59:36 +0530

    Completed 4 tabs for manage-contests (admin)

--- frontend/src/components/ManageContests/Admins/UserInfo.js
+++ frontend/src/components/ManageContests/Admins/UserInfo.js
@@ -4,23 +4,28 @@ import PropTypes from 'prop-types';
 import { Media, Input } from 'reactstrap';
 
 function UserInfo(props) {
-  const { user } = props;
-  const { isAdmin, isCreator, name, pic, username } = user;
-  console.log(user);
+  const { user, toggleAdmin } = props;
+  const { isAdmin, isCreator, id, name, pic, username } = user;
 
   return (
     <Media tag='li'>
       <Media left middle>
-        <img src={pic} alt={name} height='150px' width='150px' />
+        <img src={pic} alt={name} height='90px' width='90px' />
       </Media>
-      <Media body style={{ marginLeft: '15px' }}>
+      <Media body style={{ marginLeft: '10px' }}>
         <Media heading>{name}</Media>
         <h5 className='text-muted'>{username}</h5>
-        {isCreator ? <h4>Creator</h4> : ''}
+        {isCreator ? <h5>Creator</h5> : ''}
         {!isCreator ? (
           <>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <Input type='checkbox' checked={isAdmin} />
+            <Input
+              type='checkbox'
+              checked={isAdmin}
+              onClick={() => {
+                toggleAdmin(id);
+              }}
+            />
             Is admin
           </>
         ) : (
@@ -39,7 +44,8 @@ UserInfo.propTypes = {
     isCreator: PropTypes.bool,
     isAdmin: PropTypes.bool,
     pic: PropTypes.string
-  }).isRequired
+  }).isRequired,
+  toggleAdmin: PropTypes.func.isRequired
 };
 
 export default UserInfo;
